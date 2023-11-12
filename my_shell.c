34a70d9423f5b3066e99f8f049cb484a95a3db80
Yassine <hondayassin5@gmail.com>
2023-11-12 18:13:07 +0100

    updated

---
+++ my_shell.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+*main - Entry point of the program.
+*Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure.
+*/
+
+int main(void)
+{
+char *path_env = getenv("PATH");
+
+if (path_env == NULL)
+{
+perror("Error: PATH variable not found in the environment.");
+return (EXIT_FAILURE);
+}
+
+char *path[MAX_ARGS];
+int i = 0;
+char *token = strtok(path_env, ":");
+
+while (token != NULL && i < MAX_ARGS - 1)
+{
+path[i++] = token;
+token = strtok(NULL, ":");
+}
+path[i] = NULL;
+
+handleCommands(path);
+return (EXIT_SUCCESS);
+}
