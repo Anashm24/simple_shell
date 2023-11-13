432d50ed217bf3c7426ee4e0a7d6c4612750522b
Anashm24 <anashoumaid19@gmail.com>
2023-11-13 17:28:19 +0100

    update

--- own_functions.c
+++ own_functions.c
@@ -1,17 +1,24 @@
 #include "main.h"
 
-extern char **environ;
-int i = 0;
+/**
+ * _getenv - Retrieves the value of an environment variable.
+ * @target_key: The name of the environment variable to retrieve.
+ *
+ * Return: If the environment variable is found, returns a pointer to the
+ *         "name=value" string. Otherwise, returns NULL.
+ */
+
 char *_getenv(const char *target_key)
 {
+int i = 0;
 
-while(environ[i])
+while (environ[i])
 {
-    if (_strncmp(environ[i], target_key, _strlen(target_key)) == 0)
-    {
-        return (environ[i]);
-    }
-    i++;
+if (_strncmp(environ[i], target_key, _strlen(target_key)) == 0)
+{
+return (environ[i]);
+}
+i++;
 }
 return (NULL);
 }
