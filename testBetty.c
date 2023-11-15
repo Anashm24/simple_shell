76418068feb304342640886c3c881d8df8f077eb
Yassine <hondayassin5@gmail.com>
2023-11-15 01:52:48 +0100

    bettyTest

--- testBetty.c
+++ testBetty.c
@@ -4,25 +4,11 @@
 
 /**
 * main - Entry point of the program.
-* Description: This function tokenizes a string
-* @void: No input parameter.
 * Return: Always 0 (Success).
 */
 
 int main(void)
 {
-char str[] = "exit 98";
-char *token;
-int i = 0, x = 0;
-char **args = malloc(sizeof(char *) * 2);
-
-token = strtok(str, " ");
-
-while (token)
-{
-args[x] = token;
-x++;
-token = strtok(NULL, " ");
-}
+printf("Hello world!")
 return (0);
 }
