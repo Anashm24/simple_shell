bf3d5cc1ae9102235760c83ca88a9b3929a05e0d
Yassine <hondayassin5@gmail.com>
2023-11-18 00:13:24 +0100

    updated

--- shell.c
+++ shell.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	size_t size_line = 0;
-	char *line = NULL;
-	int status = 0;
+size_t size_line = 0;
+char *line = NULL;
+int status = 0;
 
 if (!isatty(0))
 {
@@ -27,80 +27,67 @@ _myshell();
 return (0);
 }
 /**
- * non_interactive_mode - Executes shell commands in non_nteractive_mode
+ * non_inter_md - Executes shell commands in non_nteractive_mode
  * @token: The string containing commands separated by newline characters
  * @status: integer store the number
  * Return: Returns status.
  */
 void non_inter_md(char *token, int *status)
 {
-	char **single_command;
-	char *envp[] = {NULL};
+char **single_command;
+char *envp[] = {NULL};
 
-	token[strlen(token) - 1] = '\0';
-	single_command = split_cmd(token, " \t");
-	if (single_command[0])
-	{
-	 if (!_strcmp(single_command[0], "exit"))
-	{
-			if (single_command[1])
-			{
-				int my_status = str_to_int(single_command[1]);
-
-				exit_status(my_status, single_command, &token, status);
-			}
-			else
-			{
-				free(token);
-				free_array(single_command);
-				exit(*status);
-			}
-		}
-		else if (!_strcmp(single_command[0], "env"))
-		{
-			print_env_var();
-			*status = 0;
-		}
-		else if (!_strcmp(single_command[0], "setenv"))
-		{
-            if (single_command[1] && single_command[2])
-			{
-                _setenv(single_command[1], single_command[2]);
-            }
-			else
-			{
-                fprintf(stderr, "setenv: Invalid syntax\n");
-            }
-        }
-		else if (!_strcmp(single_command[0], "unsetenv"))
-		{
-            if (single_command[1])
-			{
-                _unsetenv(single_command[1]);
-            }
-			else
-			{
-                fprintf(stderr, "unsetenv: Missing variable\n");
-            }
-        }
-		else
-		_execvep(single_command, envp, status);
-	}
-	free_array(single_command);
+token[strlen(token) - 1] = '\0';
+single_command = split_cmd(token, " \t");
+if (single_command[0])
+{
+if (!_strcmp(single_command[0], "exit"))
+{
+if (single_command[1])
+{
+int my_status = str_to_int(single_command[1]);
+exit_status(my_status, single_command, &token, status);
+}
+else
+{
+free(token);
+free_array(single_command);
+exit(*status);
+}
+}
+else if (!_strcmp(single_command[0], "env"))
+{
+print_env_var();
+*status = 0;
+}
+else if (!_strcmp(single_command[0], "cd"))
+{
+if (single_command[1] && cd_builtin(single_command[1]) != 0)
+*status = 2;
+else
+{
+if (cd_builtin(NULL) != 0)
+*status = 2;
+}
+}
+else
+_execvep(single_command, envp, status);
+}
+free_array(single_command);
 }
 
 /**
-* **split_cmd - Splits a string into tokens
-* @str: The string to tokenize
-* @delimiters: The delimiters to use for tokenization
-*
-* Return: Returns result.
-*/
+ * **split_cmd - Splits a string into tokens
+ * @str: The string to tokenize
+ * @delimiters: The delimiters to use for tokenization
+ *
+ * Return: Returns result.
+ */
 char **split_cmd(char *str, char *delimiters)
 {
-	int count = 0;
-	char *token;
-	char **result = malloc(20 * sizeof(char *));
+int count = 0;
+char *token;
+char **result = malloc(20 * sizeof(char *));
 
 if (result == NULL)
 {
